bf8f284b6b768cec25418df876317dec018948d9
Atheesh Alva <100546505+Atheesh-Alva@users.noreply.github.com>
2024-06-14 12:52:37 +0100

    Create file

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String _test = '';
     return MaterialApp(
       title: 'Playground',
       theme: ThemeData(

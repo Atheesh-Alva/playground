9ca0c023d77a6d52cfeb79a385a098e585087ad0
Atheesh Alva <100546505+Atheesh-Alva@users.noreply.github.com>
2024-06-19 11:07:26 +0100

    Coping schema

--- lib/presentation/views/home_page.dart
+++ lib/presentation/views/home_page.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:playground/data/schema_parser.dart';
+import 'package:playground/raw/mode.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -11,7 +14,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Map<String, dynamic> _data = {};
+  List<Map<String, dynamic>> _data = [];
 
   @override
   void initState() {
@@ -21,23 +24,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _loadJsonData() async {
     final String jsonString = await rootBundle.loadString(
-      'assets/raw/schema.json',
+      AppMode.developerMode() ? 'assets/raw/dev/schema.json' : 'assets/raw/release/schema.json',
     );
-    // final dataInBytes = base64Decode(jsonString);
-    // final jsonData = jsonDecode(utf8.decode(dataInBytes));
-    if (mounted) {
+    if (mounted && jsonString.isNotEmpty) {
       setState(() {
-        _data = jsonDecode(jsonString);
+        _data = SchemaParser.fetchAppSyncApis(schema: jsonDecode(jsonString));
       });
     }
   }
 
-  Widget _designText({required String str}) => ListTile(
-        title: Text(
-          str,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+  Widget _designText({required Map<String, dynamic> api}) => InkWell(
+        onTap: () => log(api["api"]),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 4.0),
+          decoration: new BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            border: Border.all(
+              color: Colors.purple,
+            ),
+            color: Colors.white,
+          ),
+          child: Column(
+            children: [
+              Text(
+                api["apiName"],
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                'Input Parameters',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                'Output Parameters:',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ],
           ),
         ),
       );
@@ -46,19 +76,20 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: (_data.isEmpty)
-            ? Theme.of(context).colorScheme.inversePrimary
-            : _data["colour"] ?? false
-                ? Colors.greenAccent
-                : Colors.redAccent,
+        backgroundColor: (_data.isEmpty) ? Theme.of(context).colorScheme.inversePrimary : Colors.greenAccent,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: const Icon(
+              Icons.info,
+              color: Colors.black,
+            ),
+          ),
+        ],
       ),
       body: Container(
-        color: (_data.isEmpty)
-            ? Theme.of(context).colorScheme.inversePrimary
-            : _data["colour"] ?? false
-                ? Colors.greenAccent.shade100
-                : Colors.redAccent.shade100,
+        color: (_data.isEmpty) ? Theme.of(context).colorScheme.inversePrimary : Colors.greenAccent.shade100,
         child: ListView(
           children: [
             const Center(
@@ -69,20 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
               physics: const ScrollPhysics(),
               itemCount: _data.length,
               itemBuilder: (_, int index) {
-                var first = _data[_data.keys.toList()[index]];
-                return (first.runtimeType == String)
-                    ? _designText(str: first)
-                    : (first.runtimeType == List)
-                        ? ListView.builder(
-                            shrinkWrap: true,
-                            physics: const ScrollPhysics(),
-                            itemCount: first.length,
-                            itemBuilder: (_, int nextIndex) {
-                              return _designText(
-                                  str: first[nextIndex].toString());
-                            },
-                          )
-                        : _designText(str: first.toString());
+                return _designText(api: _data[index]);
               },
             ),
           ],
